328a8ec9e530f204ece6f54fc0b4ee11a3f2ac0a
Felix Peters <felix.peters@breuninger.de>
2022-12-09 14:04:34 +0100

    chore: Obey the linter

--- examples/kvm/prepare.sh
+++ examples/kvm/prepare.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 podman run --pull=always --rm -v "$PWD/images":/data:rw,z -w /data \
-    quay.io/coreos/coreos-installer:release download -s "stable" -p qemu -f qcow2.xz --decompress
+  quay.io/coreos/coreos-installer:release download -s "stable" -p qemu -f qcow2.xz --decompress
