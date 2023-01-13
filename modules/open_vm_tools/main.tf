a07bbbf822fa5b3f7c4551b1c2f29ba49a12a526
Schmidt, Sascha (sasschmidt) <sascha.schmidt@breuninger.de>
2023-01-13 13:58:19 +0100

    chore: Use pod manifest to deploy open vm tools

--- modules/open_vm_tools/main.tf
+++ modules/open_vm_tools/main.tf
@@ -1,19 +1,9 @@
-locals {
-  butane = {
-    variant = "fcos"
-    version = "1.4.0"
-    systemd = {
-      units = [
-        {
-          name     = "open-vm-tools.service"
-          enabled  = true
-          contents = file(format("%s/open-vm-tools.service", path.module))
-        }
-      ]
-    }
-  }
+module "pod" {
+  source   = "../pod"
+  name     = "open-vm-tools"
+  manifest = file(format("%s/manifest.yml", path.module))
 }
 
 output "butane" {
-  value = yamlencode(local.butane)
+  value = module.pod.butane
 }
