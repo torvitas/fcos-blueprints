1098638507d54a9ab0c3cd79766c2e8825f946e1
Schmidt, Sascha (sasschmidt) <sascha.schmidt@breuninger.de>
2023-01-20 12:33:12 +0100

    feat: Change name of pod to comply with naming rules of pod module

--- modules/open_vm_tools/main.tf
+++ modules/open_vm_tools/main.tf
@@ -10,9 +10,10 @@
  * The pod definition is roughly based on this blog post:
  * <https://developers.redhat.com/blog/2017/03/23/containerizing-open-vm-tools-part-1-the-dockerfile-and-constructing-a-systemd-unit-file>
  */
+
 module "pod" {
   source   = "../pod"
-  name     = "open-vm-tools"
+  name     = "open_vm_tools"
   manifest = file(format("%s/manifest.yml", path.module))
 }
 
