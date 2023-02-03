1eaf15c2dddd74c595b77536fed20ea72de5a610
Schmidt, Sascha (sasschmidt) <sascha.schmidt@breuninger.de>
2023-02-03 19:08:32 +0100

    chore: Remove obsolete files

--- examples/aws/main.tf
+++
@@ -1,1 +0,0 @@
-# deploy to ec2
--- examples/kvm/prepare.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-podman run --pull=always --rm -v "$PWD/images":/data:rw,z -w /data \
-  quay.io/coreos/coreos-installer:release download -s "stable" -p qemu -f qcow2.xz --decompress
